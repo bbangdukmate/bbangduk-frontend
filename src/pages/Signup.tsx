5499f857dbedf16d8b65e96f6bc905b8d1fb893a
2taesung <dlxotjde@gmail.com';;Sz>
2024-07-16 01:27:31 +0900

    feat: 회원가입 화면 커스텀 디자인 디테일들

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
   StyleSheet,
   useColorScheme,
   TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Image,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { RootStackParamList } from '../../App';
 
 type SectionProps = {
   title: string;
 };
 
-function Section({ title }: SectionProps): React.JSX.Element {
+export function Section({ title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -32,124 +37,287 @@ function Section({ title }: SectionProps): React.JSX.Element {
 }
 
 function Signup(): React.JSX.Element {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [nickname, setNickname] = useState('');
+  const [birthday, setBirthday] = useState('');
   const [gender, setGender] = useState('');
   const [termsAccepted, setTermsAccepted] = useState(false);
+  const [personalInfoAccepted, setPersonalInfoAccepted] = useState(false);
+  const [ageAccepted, setAgeAccepted] = useState(false);
+  const [marketingAccepted, setMarketingAccepted] = useState(false);
+  const [allAgree, setAllAgree] = useState(false);
+  const [pushAccepted, setPushAccepted] = useState(false);
+  const [emailAccepted, setEmailAccepted] = useState(false);
+
+
+  useEffect(() => {
+    if (marketingAccepted) {
+      setPushAccepted(true)
+      setEmailAccepted(true)
+    } else {
+      setPushAccepted(false)
+      setEmailAccepted(false)
+    }
+  }, [marketingAccepted])
+
+  useEffect(() => {
+    if (allAgree) {
+      setTermsAccepted(true)
+      setPersonalInfoAccepted(true)
+      setAgeAccepted(true)
+      setMarketingAccepted(true)
+    } else {
+      setTermsAccepted(false)
+      setPersonalInfoAccepted(false)
+      setAgeAccepted(false)
+      setMarketingAccepted(false)
+    }
+  }, [allAgree])
 
   return (
-    <View style={styles.container}>
-      <Section title='회원가입' />
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>회원 정보</Text>
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>이름</Text>
-          <Text style={styles.required}>*</Text>
-        </View>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholder="홍길동"
-        />
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>휴대전화번호</Text>
-          <Text style={styles.required}>*</Text>
-        </View>
-        <View style={styles.rowContainer}>
-          <TextInput
-            style={styles.leftInput}
-            value={name}
-            onChangeText={setName}
-            keyboardType="phone-pad"
-            placeholder="01000000000"
-          />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>인증번호 요청</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>아이디(이메일)</Text>
-          <Text style={styles.required}>*</Text>
-        </View>
-
-        <View style={styles.rowContainer}>
-          <TextInput
-            style={styles.leftInput}
-            value={email}
-            onChangeText={setEmail}
-            placeholder="hong6@naver.com"
-            keyboardType="email-address"
-          />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>중복확인</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>닉네임</Text>
-          <Text style={styles.required}>*</Text>
-        </View>
-
-        <View style={styles.rowContainer}>
-          <TextInput
-            style={styles.leftInput}
-            value={nickname}
-            onChangeText={setNickname}
-            placeholder="홍길동1호"
-          />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>중복확인</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={styles.label}>성별</Text>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity style={styles.radioContainer} onPress={() => setGender('male')}>
-            <View style={styles.radio}>
-              {gender === 'male' && <View style={styles.radioSelected} />}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={true}>
+        <View style={styles.container}>
+          <Section title='회원가입' />
+          <View style={styles.inputContainer}>
+            <Text style={styles.semiTitleLabel}>회원 정보</Text>
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>이름</Text>
+              <Text style={styles.required}>*</Text>
             </View>
-            <Text style={styles.radioLabel}>남성</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.radioContainer} onPress={() => setGender('female')}>
-            <View style={styles.radio}>
-              {gender === 'female' && <View style={styles.radioSelected} />}
+            <TextInput
+              style={styles.input}
+              value={name}
+              onChangeText={setName}
+              placeholder="홍길동"
+            />
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>휴대전화번호</Text>
+              <Text style={styles.required}>*</Text>
             </View>
-            <Text style={styles.radioLabel}>여성</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.labelContainer}>
-          <Text style={styles.label}>전체 동의</Text>
-          <Text style={styles.required}>*</Text>
-        </View>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity
-            style={styles.checkboxContainer}
-            onPress={() => setTermsAccepted(!termsAccepted)}
-          >
-            <View style={styles.checkbox}>
-              {termsAccepted && <View style={styles.checkboxSelected} />}
+            <View style={styles.rowContainer}>
+              <TextInput
+                style={styles.leftInput}
+                value={name}
+                onChangeText={setName}
+                keyboardType="phone-pad"
+                placeholder="01000000000"
+              />
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>인증번호 요청</Text>
+              </TouchableOpacity>
             </View>
-            <Text style={styles.checkboxLabel}>이용약관 동의(필수)</Text>
+            <Text style={styles.info}>* 휴대전화번호는 숫자만 입력 가능합니다.</Text>
+
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>아이디(이메일)</Text>
+              <Text style={styles.required}>*</Text>
+            </View>
+
+            <View style={styles.rowContainer}>
+              <TextInput
+                style={styles.leftInput}
+                value={email}
+                onChangeText={setEmail}
+                placeholder="hong6@naver.com"
+                keyboardType="email-address"
+              />
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>중복확인</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>닉네임</Text>
+              <Text style={styles.required}>*</Text>
+            </View>
+
+            <View style={styles.rowContainer}>
+              <TextInput
+                style={styles.leftInput}
+                value={nickname}
+                onChangeText={setNickname}
+                placeholder="홍길동1호"
+              />
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>중복확인</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.info}>닉네임은 국문, 영문, 숫자만 사용하고 2~10글자까지 가능합니다.</Text>
+            <Text style={styles.semiTitleLabel}>추가 정보</Text>
+            <Text style={styles.label}>성별</Text>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity style={styles.radioContainer} onPress={() => setGender('male')}>
+
+                {
+                  gender === 'male' 
+                    ?<Image
+                    source={require('../assets/Ellipse_activate.png')}/>
+                    :<Image
+                    source={require('../assets/Ellipse_inactivate.png')}/>
+                }
+                <Text style={styles.radioLabel}>남</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.radioContainer} onPress={() => setGender('female')}>
+              {
+                  gender === 'female' 
+                    ?<Image
+                    source={require('../assets/Ellipse_activate.png')}/>
+                    :<Image
+                    source={require('../assets/Ellipse_inactivate.png')}/>
+                }
+                <Text style={styles.radioLabel}>여</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.labelContainer}>
+              <Text style={styles.label}>생년월일</Text>
+            </View>
+            <TextInput
+              style={styles.input}
+              value={birthday}
+              onChangeText={setBirthday}
+              placeholder="20000101"
+            />
+            <Text style={styles.info}>*숫자로 8자리까지 입력 가능합니다.</Text>
+            
+            <View style={styles.allAgree}>
+              <TouchableOpacity
+                style={styles.checkboxContainer}
+                onPress={() => setAllAgree(!allAgree)}
+              >
+
+              {
+                allAgree 
+                  ?<Image
+                  style={styles.checkboxIcon}
+                  source={require('../assets/checkbox_activate.png')}/>
+                  :<Image
+                  style={styles.checkboxIcon}
+                  source={require('../assets/checkbox_inactivate.png')}/>
+              }
+              <Text style={styles.label}>전체 동의</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity
+                style={styles.checkboxContainer}
+                onPress={() => setTermsAccepted(!termsAccepted)}
+              >
+                {
+                  termsAccepted 
+                    ?<Image
+                    source={require('../assets/check_activate.png')}/>
+                    :<Image
+                    source={require('../assets/check_inactivate.png')}/>
+                }
+                <Text style={styles.checkboxLabel}>이용약관 동의</Text>
+                <Text style={styles.checkboxRequired}>(필수)</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity
+                style={styles.checkboxContainer}
+                onPress={() => setPersonalInfoAccepted(!personalInfoAccepted)}
+              >
+                {
+                  personalInfoAccepted 
+                    ?<Image
+                    source={require('../assets/check_activate.png')}/>
+                    :<Image
+                    source={require('../assets/check_inactivate.png')}/>
+                }
+                <Text style={styles.checkboxLabel}>개인정보 수집 및 이용 동의</Text>
+                <Text style={styles.checkboxRequired}>(필수)</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity
+                style={styles.checkboxContainer}
+                onPress={() => setAgeAccepted(!ageAccepted)}
+              >
+                {
+                  ageAccepted 
+                    ?<Image
+                    source={require('../assets/check_activate.png')}/>
+                    :<Image
+                    source={require('../assets/check_inactivate.png')}/>
+                }
+                <Text style={styles.checkboxLabel}>만 14세 이상 확인</Text>
+                <Text style={styles.checkboxRequired}>(필수)</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.info}>*14세 미만 회원의 가입은 불가합니다.</Text>
+            <View style={styles.rowContainer}>
+              <TouchableOpacity
+                style={styles.checkboxContainer}
+                onPress={() => setMarketingAccepted(!marketingAccepted)}
+              >
+                {
+                  marketingAccepted 
+                    ?<Image
+                    source={require('../assets/check_activate.png')}/>
+                    :<Image
+                    source={require('../assets/check_inactivate.png')}/>
+                }
+                <View>
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.checkboxLabel}>마케팅 수신 동의</Text>
+                    <Text style={styles.checkboxNotRequired}>(선택)</Text>
+                  </View>
+                  <View style={styles.rowContainer}>
+                    <TouchableOpacity style={styles.radioContainer} onPress={() => setPushAccepted(!pushAccepted)}>
+                      {
+                        pushAccepted
+                          ?<Image
+                          source={require('../assets/checkbox_activate.png')}/>
+                          :<Image
+                          source={require('../assets/checkbox_inactivate.png')}/>
+                      }
+                      <Text style={styles.radioLabel}>푸시</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.radioContainer} onPress={() => setEmailAccepted(!emailAccepted)}>
+                      {
+                        emailAccepted
+                          ?<Image
+                          source={require('../assets/checkbox_activate.png')}/>
+                          :<Image
+                          source={require('../assets/checkbox_inactivate.png')}/>
+                      }
+                      <Text style={styles.radioLabel}>E-mail</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <TouchableOpacity style={styles.lastButton} onPress={() => navigation.navigate('Profile')}>
+            <Text style={styles.lastButtonText}>완료</Text>
           </TouchableOpacity>
+          <View style={styles.spacer} />
         </View>
-      </View>
-
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>회원가입</Text>
-      </TouchableOpacity>
-      <View style={styles.spacer} />
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  scrollContainer: {
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  container: {
+    width: '100%',
+    alignItems: 'center',
     backgroundColor: Colors.white,
+    paddingVertical: 20,
   },
   sectionContainer: {
     marginTop: 32,
@@ -168,19 +336,30 @@ const styles = StyleSheet.create({
   required: {
     color: 'red',
     marginLeft: 4,
-    marginBottom: 15,
   },
   inputContainer: {
     width: '80%',
     marginTop: 16,
   },
+  semiTitleLabel: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 30,
+  },
   label: {
     fontSize: 16,
     fontWeight: '600',
+    marginTop: 10,
+  },
+  info: {
+    fontSize: 10,
+    fontWeight: '400',
     marginBottom: 15,
+    color: '#EC5938',
   },
   leftInput: {
-    height: 50,
+    height: 40,
     flex: 1,
     marginRight: 8,
     borderColor: Colors.light,
@@ -199,7 +378,7 @@ const styles = StyleSheet.create({
   rowContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 16,
+    marginTop: 10,
   },
   button: {
     width: 120,
@@ -240,6 +419,7 @@ const styles = StyleSheet.create({
   },
   radioLabel: {
     fontSize: 16,
+    marginLeft: 10,
   },
   checkboxContainer: {
     flexDirection: 'row',
@@ -262,7 +442,35 @@ const styles = StyleSheet.create({
   checkboxLabel: {
     fontSize: 16,
   },
+  checkboxRequired: {
+    color: '#EC5938'
+  },
+  checkboxNotRequired: {
+    color: '#AEAEB0'
+  },
+  checkboxIcon: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  allAgree: {
+    backgroundColor: '#F9FAFC',
+    display: 'flex',
+    flexDirection: 'row',
+    height: 40,
+  },
+  lastButton: {
+    width: '100%',
+    height: 80,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',  
+    backgroundColor: '#EC5938',
+  },
+  lastButtonText: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: '#FFFFFF'
+  }
 });
-
 
 export default Signup;
